cf4f5e5d4534617d4c3454dc8bd2f7f6a49073ff
Hrtsuna24 <149830352+hrtsnmi@users.noreply.github.com>
2024-03-20 22:27:35 +0200

    End Task4

--- Source/APITest/Public/AI/TAAIBaseCharacter.h
+++ Source/APITest/Public/AI/TAAIBaseCharacter.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
+
 #include "TAAIBaseCharacter.generated.h"
 
 
@@ -13,6 +15,23 @@ UCLASS()
 class APITEST_API ATAAIBaseCharacter : public ACharacter
 {
 	GENERATED_BODY()
+
+private:
+    /** MappingContext */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    class UInputMappingContext* DefaultMappingContext;
+
+    /** Move Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    class UInputAction* MoveAction;
+
+    /** Look Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    class UInputAction* LookAction;
+
+private:
+    void Move(const FInputActionValue& Value);
+    void Look(const FInputActionValue& Value);
 
 public:
 	ATAAIBaseCharacter();
