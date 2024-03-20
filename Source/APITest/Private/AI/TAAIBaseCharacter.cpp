6c9f8477c0056676b043a20d3fc52f932f1d143b
Hrtsuna24 <149830352+hrtsnmi@users.noreply.github.com>
2024-03-20 22:21:05 +0200

    End Task4

--- Source/APITest/Private/AI/TAAIBaseCharacter.cpp
+++ Source/APITest/Private/AI/TAAIBaseCharacter.cpp
@@ -3,6 +3,43 @@
 
 #include "AI/TAAIBaseCharacter.h"
 #include "QuestSystem/TAQuestComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+void ATAAIBaseCharacter::Move(const FInputActionValue& Value)
+{
+    if (!Controller)
+    {
+        return;
+    }
+
+    const FVector2D MovementVector = Value.Get<FVector2D>();
+    const FRotator YawRotation(0, GetControlRotation().Yaw, 0);
+    FVector MoveDirection;
+
+    if (MoveDirection.Y != 0.f)
+    {
+        MoveDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+        AddMovementInput(MoveDirection, MovementVector.Y);
+    }
+    if (MoveDirection.X != 0.f)
+    {
+        MoveDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+        AddMovementInput(MoveDirection, MovementVector.X);
+    }
+}
+
+void ATAAIBaseCharacter::Look(const FInputActionValue& Value)
+{
+    FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+    if (Controller != nullptr)
+    {
+        // add yaw and pitch input to controller
+        AddControllerYawInput(LookAxisVector.X);
+        AddControllerPitchInput(-LookAxisVector.Y);
+    }
+}
 
 ATAAIBaseCharacter::ATAAIBaseCharacter()
 {
@@ -26,7 +63,16 @@ void ATAAIBaseCharacter::Tick(float DeltaTime)
 
 void ATAAIBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    APlayerController* PlayerController = Cast<APlayerController>(GetController());
 
+    UEnhancedInputLocalPlayerSubsystem* Subsystem =
+        ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+
+    Subsystem->ClearAllMappings();
+    Subsystem->AddMappingContext(DefaultMappingContext, 0);
+
+    UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+    EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATAAIBaseCharacter::Move);
+    EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ATAAIBaseCharacter::Look);
 }
 
