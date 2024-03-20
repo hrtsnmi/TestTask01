7a452d470b44889352a54d782c8c295b1aae7846
Hrtsuna24 <149830352+hrtsnmi@users.noreply.github.com>
2024-03-20 22:34:33 +0200

    Fix

--- Source/APITest/Private/AI/TAAIBaseCharacter.cpp
+++ Source/APITest/Private/AI/TAAIBaseCharacter.cpp
@@ -3,43 +3,6 @@
 
 #include "AI/TAAIBaseCharacter.h"
 #include "QuestSystem/TAQuestComponent.h"
-#include "EnhancedInputComponent.h"
-#include "EnhancedInputSubsystems.h"
-
-void ATAAIBaseCharacter::Move(const FInputActionValue& Value)
-{
-    if (!Controller)
-    {
-        return;
-    }
-
-    const FVector2D MovementVector = Value.Get<FVector2D>();
-    const FRotator YawRotation(0, GetControlRotation().Yaw, 0);
-    FVector MoveDirection;
-
-    if (MoveDirection.Y != 0.f)
-    {
-        MoveDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-        AddMovementInput(MoveDirection, MovementVector.Y);
-    }
-    if (MoveDirection.X != 0.f)
-    {
-        MoveDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-        AddMovementInput(MoveDirection, MovementVector.X);
-    }
-}
-
-void ATAAIBaseCharacter::Look(const FInputActionValue& Value)
-{
-    FVector2D LookAxisVector = Value.Get<FVector2D>();
-
-    if (Controller != nullptr)
-    {
-        // add yaw and pitch input to controller
-        AddControllerYawInput(LookAxisVector.X);
-        AddControllerPitchInput(-LookAxisVector.Y);
-    }
-}
 
 ATAAIBaseCharacter::ATAAIBaseCharacter()
 {
@@ -61,18 +24,4 @@ void ATAAIBaseCharacter::Tick(float DeltaTime)
 
 }
 
-void ATAAIBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-    APlayerController* PlayerController = Cast<APlayerController>(GetController());
-
-    UEnhancedInputLocalPlayerSubsystem* Subsystem =
-        ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
-
-    Subsystem->ClearAllMappings();
-    Subsystem->AddMappingContext(DefaultMappingContext, 0);
-
-    UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
-    EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATAAIBaseCharacter::Move);
-    EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ATAAIBaseCharacter::Look);
-}
 
--- Source/APITest/Public/AI/TAAIBaseCharacter.h
+++ Source/APITest/Public/AI/TAAIBaseCharacter.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "InputActionValue.h"
+
 
 #include "TAAIBaseCharacter.generated.h"
 
@@ -16,29 +16,10 @@ class APITEST_API ATAAIBaseCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
-private:
-    /** MappingContext */
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-    class UInputMappingContext* DefaultMappingContext;
-
-    /** Move Input Action */
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-    class UInputAction* MoveAction;
-
-    /** Look Input Action */
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-    class UInputAction* LookAction;
-
-private:
-    void Move(const FInputActionValue& Value);
-    void Look(const FInputActionValue& Value);
-
 public:
 	ATAAIBaseCharacter();
 
 	virtual void Tick(float DeltaTime) override;
-
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 
